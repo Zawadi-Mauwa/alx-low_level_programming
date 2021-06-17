23d02499f5bdf5c7bc42e6f024fbb527ec2eefc5
root <www.mmolalugi@gmail.com>
2021-06-17 12:58:39 -0700

    pushing my 4. Hello, puts task for submission

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
 #include <stdio.h>
 
-/*
-This is a C Program that prints a string 
-It retuens an integer value of 0
-It doesn't use the printf function but instared it uses the puts function
-*/
+/*This is a C Program that prints a string, it retuens an integer value of 0, it doesn't use the printf function but instared it uses the puts function*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
