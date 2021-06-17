edaaf8b75e1b6d53994d6d192c9a4e550ffb59a8
root <www.mmolalugi@gmail.com>
2021-06-17 13:20:23 -0700

    pushing my 4. Hello, puts task for submission

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- *This is a C Program that prints a string
- *It's a main function - which is entry point or point of exercution
+ *main function - which is entry point or point of exercution
+ *
  *It retuens an integer value of 0
  */
 int main(void)
