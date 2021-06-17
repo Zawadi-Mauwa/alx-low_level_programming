cd2dbc4394f9024cba21352b227a996525b9dec6
root <www.mmolalugi@gmail.com>
2021-06-17 13:11:17 -0700

    pushing my 4. Hello, puts task for submission

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
-/*This is a C Program that prints a string
- *It's a main function - which is entry point or point of exercution 
- *It retuens an integer value of 0*/
+/**
+ *This is a C Program that prints a string
+ *It's a main function - which is entry point or point of exercution
+ *It retuens an integer value of 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
