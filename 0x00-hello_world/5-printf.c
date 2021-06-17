724bd1af8bd4e7947fe1084d0a5111fba0fb68f8
root <www.mmolalugi@gmail.com>
2021-06-17 13:40:34 -0700

    pushing my 5. Hello, printf task for submission

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ *main - the entry point of exercution
+ *return - returns the value of 0
+ */
+int main(void)
+{
+  printf("with proper grammer, but the outcome is a piece of art,");
+  return(0);
+}
