6e5a21de6756061de62de0dd6d25e492643d4fbf
root <www.mmolalugi@gmail.com>
2021-06-21 12:17:04 -0700

    .

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point for writing entire lowercase alphabet except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int character;
+ 
+for (character = 'a' ; character <= 'z' ; character++)
+{
+if (character != 'q' && character != 'e')
+{
+putchar(character);
+}
+}
+putchar('\n');
+return (0);
+}
