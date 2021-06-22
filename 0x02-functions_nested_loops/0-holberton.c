a7e364a2975d670117a3ec6fb2090fb06b1b4343
root <www.mmolalugi@gmail.com>
2021-06-22 11:13:25 -0700

    Pushing my 0. _puchar task for submission

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - point of entry or point of exercution.
+ *
+ *Return: must always return 0 in the case of successful exercution.
+ *
+ */
+
+int main(void)
+{
+printf("%s", "Holberton\n")
+return (0);
+}
