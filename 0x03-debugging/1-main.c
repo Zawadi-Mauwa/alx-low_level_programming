8511aab655ead0ff857dd9284e49f349d8fd621f
root <www.mmolalugi@gmail.com>
2021-06-25 13:29:44 -0700

    Pushing my . Like, comment, subscribe task for submission

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Should create or mare an infinite loop
+ *
+ * Return: Should always return 0 un the case of success
+ */
+
+int main(void)
+{
+int i;
+
+printf("Infinite loop incoming :(\n");
+
+i = 0;
+/*
+*while (i < 10)
+*{
+*putchar(i);
+*}
+*/
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
+}
