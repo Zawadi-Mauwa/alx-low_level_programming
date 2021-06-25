1093a42e758b50420f36d457d23b67be09779073
root <www.mmolalugi@gmail.com>
2021-06-25 13:22:47 -0700

    Pushing my 0.Multiple mains task for submission

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *
+ * main - Should check or test function for positive or negative
+ *
+ * Return: should always return 0 in tbe case of success
+ */
+
+int main(void)
+{
+int i;
+
+i = 0;
+positive_or_negative(i);
+return (0);
+}
