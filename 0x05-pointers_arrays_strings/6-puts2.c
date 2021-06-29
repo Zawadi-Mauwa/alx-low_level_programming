e7a63a2a439ceb8a9f0aa873316f9399033cb294
root <www.mmolalugi@gmail.com>
2021-06-29 03:48:17 -0700

    Pushing my 6. task for submission

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- *puts2 - Will print every other character of a string & start with first character
+ *puts2 - Prints every other char of a string & start with first char
  *
  *@str: Its the only parameter of this function
  *
@@ -13,7 +13,7 @@ int i = 0;
 
 while (str[i] != '\0')
 {
-if (i % 2 ==0)
+if (i % 2 == 0)
 {
 _putchar(str[i]);
 }
