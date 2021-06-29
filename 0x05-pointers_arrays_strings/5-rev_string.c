bd1334188704ad60921a17d5a815c669794a1c28
root <www.mmolalugi@gmail.com>
2021-06-29 03:20:51 -0700

    Pushing my 5. task for submission

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ *rev_string - It will reverse a string
+ *
+ *@s: It's the only parameter for this function
+ *
+ *Return: should always return void
+ */
+
+void rev_string(char *s)
+{
+int i = 0;
+int j = 0;
+int temp;
+
+while (s[i] != '\0')
+{
+i++;
+}
+i--;
+
+while (i > j)
+{
+temp = s[i];
+s[i--] = s[j];
+s[a++] = temp;
+}
+}
