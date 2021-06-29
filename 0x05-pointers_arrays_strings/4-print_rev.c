cc8fed48386e159bc695484b2d77efe5cedb5efe
root <www.mmolalugi@gmail.com>
2021-06-29 02:53:09 -0700

    Pushing my 4. taask for submisshion

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_rev - Will print a sting in reverse
+ *
+ *@s: It's the only parameter in this function
+ *
+ *Return: It should always retun void
+ */
+
+void print_rev(char *s)
+{
+int i = 0
+
+while (s[i])
+i++;
+
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
