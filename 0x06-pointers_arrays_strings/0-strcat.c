96b0835a7d60372829045a2034192a4559519fdd
root <www.mmolalugi@gmail.com>
2021-07-01 11:43:56 -0700

    Pushing my 0. strcat task for submission.

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/** 
+* _strcat - appends string 2 to string 1
+*
+* @dest: 1st param & pointer to destination string
+* 
+* @src: 2nd param & pointer to source string 
+*
+* Return: Retuns pointer to resulting string dest 
+*/
+char *_strcat(char *dest, char *src)
+{
+int i, j;
+i = 0;
+for (j = 0; dest[j] != '\0'; j++);
+while (src[i] != '\0')
+{
+dest[j + i] = src[i];
+i++;
+}
+return (dest);
+}
