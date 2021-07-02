0e583bf4b3f5affcd3c971a040c0e79ac4d39375
root <www.mmolalugi@gmail.com>
2021-07-02 11:14:43 -0700

    .

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,8 +13,9 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j; i = 0;
-for (j = 0; dest[j] != '\0'; j++);
+int i, j;
+i = 0;
+for (j = 0; dest[j] != '\0'; j++)
 while (src[i] != '\0' && i < n)
 {
 dest[j + i] = src[i];
