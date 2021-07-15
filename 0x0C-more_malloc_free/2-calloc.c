59d000bfb807f3585a5c00f88fdd2ad073e41a9d
root <www.mmolalugi@gmail.com>
2021-07-15 10:30:42 -0700

    Pushing my 1. string_nconcat for submission.

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,12 +13,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *p;
 unsigned int i;
-
 if (nmemb == 0 || size == 0)
 return (0);
-
 p = malloc(nmemb * size);
-
 if (p == 0)
 return (0);
 for (i = 0; i < (nmemb * size); i++)
