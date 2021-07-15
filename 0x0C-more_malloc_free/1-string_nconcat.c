534126979d30f4fc5928b1e4774268960b9823b2
root <www.mmolalugi@gmail.com>
2021-07-15 09:41:55 -0700

    Pushing my 1. string_nconcat for submission.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 unsigned int i, j, size, len1, len2;
 char *ptr;
-
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
@@ -35,7 +34,6 @@ ptr[i] = s2[j];
 ptr[i] = 0;
 return (ptr);
 }
-
 /**
  * _strlen - Will return length of a string
  *
@@ -46,7 +44,6 @@ return (ptr);
 int _strlen(char *s)
 {
 char *p = s;
-
 while (*s)
 s++;
 return (s - p);
