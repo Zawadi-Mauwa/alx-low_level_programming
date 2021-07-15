68afcaf6f4bc1b002e6c9b5caa33a82f590f3b53
root <www.mmolalugi@gmail.com>
2021-07-15 09:26:06 -0700

    Pushing my 1. string_nconcat for submission.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,27 +13,27 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  unsigned int i, j, size, len1, len2;
-  char *ptr;
+unsigned int i, j, size, len1, len2;
+char *ptr;
 
-  if (s1 == 0)
-  s1 = "";
-  if (s2 == 0)
-  s2 = "";
-  len1 = _strlen(s1);
-  len2 = _strlen(s2);
-  if (n > len2)
-  n = len2;
-  size = len1 + n;
-  ptr = malloc(sizeof(char) * size + 1);
-  if (!ptr)
-  return (0);
-  for (j = 0; j < n; j++, i++)
-  ptr[i] = s2[j];
-  for (i = 0; i < len1; i++)
-  ptr[i] = s1[i];
-  ptr[i] = 0;
-  return (ptr);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+len1 = _strlen(s1);
+len2 = _strlen(s2);
+if (n > len2)
+n = len2;
+size = len1 + n;
+ptr = malloc(sizeof(char) * size + 1);
+if (!ptr)
+return (NULL);
+for (j = 0; j < n; j++, i++)
+ptr[i] = s2[j];
+for (i = 0; i < len1; i++)
+ptr[i] = s1[i];
+ptr[i] = 0;
+return (ptr);
 }
 
 /**
