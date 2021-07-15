45d94adbab89193ff1574531e98442c7ef936ab3
root <www.mmolalugi@gmail.com>
2021-07-15 10:05:10 -0700

    Pushing my 1. string_nconcat for submission.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,21 +11,17 @@
  *
  * Return: should return concatenated/combined string
  **/
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *ptr;
 unsigned int len = n, i;
-
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 for (i = 0; s1[i]; i++)
 len++;
-
 ptr = malloc(sizeof(char) * (len + 1));
-
 if (ptr == NULL)
 return (NULL);
 len = 0;
