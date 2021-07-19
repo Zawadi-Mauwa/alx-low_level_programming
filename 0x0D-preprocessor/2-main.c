120351d8b8aef2f8a2207b0f4c1c5d6d487e9538
root <www.mmolalugi@gmail.com>
2021-07-19 11:13:16 -0700

    Pushing my 1.PI and my 2.File name files for submission.

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - will Print the name of the file the program was compiled from
+ *
+ * Return: Should always return 0 in case of sucsess
+ */
+
 int main(void)
 {
 prinf("%s\n", __FILE__);
