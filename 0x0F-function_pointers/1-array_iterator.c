d6b26fd8eae0d0d60c4880170ea042b4d0101509
root <www.mmolalugi@gmail.com>
2021-07-21 15:51:01 -0700

    Pushing my 1. If you spend too much time thinking about a thing, you'll never get it done task for submission.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Will ecute function as parameter on 
+ *every element of an array
+ *
+ * @array: 1st parameter & is an array of intege
+ *
+ * @size: 2nd parameter & is the size of array
+ *
+ * @action: 3rd parameter & is a function pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+if (array != NULL && action != NULL)
+{
+for (i = 0; i < size; i++)
+action(array[i]);
+}
+}
