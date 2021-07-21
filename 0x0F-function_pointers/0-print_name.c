c61b2cfcd72d860bd27d524b3e2da627502635ca
root <www.mmolalugi@gmail.com>
2021-07-21 15:24:54 -0700

    Pushing my 0. What's my name task for submission.

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,14 @@
 #include "function_pointers.h"
 /**
- * print_name - prints a name 
+ * print_name - Will print a name 
  *
- * @name: string 
+ * @name: 2nd parameter and is a string 
  *
  * @f: 2nd parameter and is a function 
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-if (f != 0)
+if (f != NULL)
 f(name);
 }
